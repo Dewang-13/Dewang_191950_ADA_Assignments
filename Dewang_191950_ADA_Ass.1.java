fcdf9041f0d5f04c7ad41cafb69e529225027736
Dewang Sharma <60025416+Dewang-creator@users.noreply.github.com>
2021-09-14 01:34:04 +0530

    Update Dewang_191950_ADA_Ass.1.java

--- Dewang_191950_ADA_Ass.1.java
+++ Dewang_191950_ADA_Ass.1.java
@@ -6,16 +6,16 @@ import java.util.*;
       // write your code here
       Scanner scn = new Scanner(System.in);
       int n = scn.nextInt();
-      
-      int a=0;  //1
-      int b=1;  //2
+      //Fibonnaci Series: 0 1 1 2 3 5 8 13 21...
+      int a=0;  //1term
+      int b=1;  //2term
       
       for(int i=1;i<=n;i++)  //3 to n
       {
           System.out.println(a);
-          int c=a+b;
-          a=b;
-          b=c;
+          int c=a+b;//3rd term
+          a=b;//storing value of 2nd term into 1st term
+          b=c;//storing value of 3rd term into 2nd term
       }
       
    }
